d057a3d43519d0bd0b38c3391bb225469acc70cf
Ryan McCarty <Ryan McCarty>
2017-04-11 13:11:22 -0500

    update to music history

---
+++ songs.js
@@ -0,0 +1,107 @@
+var songs = [];
+var metallicaSongs= [];
+var megadethSongs= [];
+var mjSongs= [];
+var eaglesSongs=[];
+/*
+songs[songs.length] = "A Light That Never Goes Out > by The Smiths on the album The Queen is Dead";
+songs[songs.length] = "Man in The Mirror > by Michael Jackson on the album Bad";
+songs[songs.length] = "Tequila Sunrise > by The Eagles on the album Desperado";
+songs[songs.length] = "Wake up Dead > by Megadeth on the album Peace Sells...but Who's Buying?";
+songs[songs.length] = "Blackened > by Metallica on the album And Justice For All";
+*/
+for (var i=0; i< 4; i++){
+	if(i ===0 ){
+		songs.push("A Light That Never Goes Out");
+		document.getElementById('smithSong').innerHTML=songs;
+	}
+	if(i ===1){
+		songs.push("by: The Smiths |");
+		document.getElementById('smithBand').innerHTML=songs[1];
+	}
+	if(i===2){
+		songs.push('on the album "The Queen is Dead" |');
+		document.getElementById('smithAlbum').innerHTML=songs[2];
+	}
+	if(i===3){
+		songs.push(' Alternative Rock');
+		document.getElementById('smithGenre').innerHTML=songs[3];
+	}
+}
+for (var i=0; i< 4; i++){
+	if(i ===0 ){
+		metallicaSongs.push("Blackened");
+		document.getElementById('metalSong').innerHTML=metallicaSongs;
+	}
+	if(i ===1){
+		metallicaSongs.push("by: Metallica |");
+		document.getElementById('metalBand').innerHTML=metallicaSongs[1];
+	}
+	if(i===2){
+		metallicaSongs.push('on "And Justic For All" |');
+		document.getElementById('metalAlbum').innerHTML=metallicaSongs[2];
+	}
+	if(i===3){
+		metallicaSongs.push(' Metal');
+		document.getElementById('metalGenre').innerHTML=metallicaSongs[3];
+	}
+}
+for (var i=0; i< 4; i++){
+	if(i ===0 ){
+		megadethSongs.push("Wake Up Dead");
+		document.getElementById('megaSong').innerHTML=megadethSongs;
+	}
+	if(i ===1){
+		megadethSongs.push("by: Megadeth |");
+		document.getElementById('megaBand').innerHTML=megadethSongs[1];
+	}
+	if(i===2){
+		megadethSongs.push("on 'Peace Sells...but Who's Buying?' |");
+		document.getElementById('megaAlbum').innerHTML=megadethSongs[2];
+	}
+	if(i===3){
+		megadethSongs.push(' Metal/Thrash');
+		document.getEleme			 ntById('megaGenre').innerHTML=megadethSongs[3];
+	}
+}
+for (var i=0; i< 4; i++){
+	if(i ===0 ){
+		mjSongs.push("Man In The Mirror");
+		document.getElementById('mjSong').innerHTML=mjSongs;
+	}
+	if(i ===1){
+		mjSongs.push("by: Michael Jackson |");
+		document.getElementById('mjArtist').innerHTML=mjSongs[1];
+	}
+	if(i===2){
+		mjSongs.push('on "Bad" |');
+		document.getElementById('mjAlbum').innerHTML=mjSongs[2];
+	}
+	if(i===3){
+		mjSongs.push(' Pop');
+		document.getElementById('mjGenre').innerHTML=mjSongs[3];
+	}
+}
+for (var i=0; i< 4; i++){
+	if(i ===0 ){
+		eaglesSongs.push("Tequila Sunrise");
+		document.getElementById('eagleSong').innerHTML=eaglesSongs;
+	}
+	if(i ===1){
+		eaglesSongs.push("by: The Eagles |");
+		document.getElementById('eagleBand').innerHTML=eaglesSongs[1];
+	}
+	if(i===2){
+		eaglesSongs.push('on "Desperado" |');
+		document.getElementById('eagleAlbum').innerHTML=eaglesSongs[2];
+	}
+	if(i===3){
+		eaglesSongs.push(' Folk/Rock');
+		document.getElementById('eagleGenre').innerHTML=eaglesSongs[3];
+	}
+}
+console.log(songs);
+console.log(metallicaSongs);
+console.log(megadethSongs);
+console.log(mjSongs);
+console.log(eaglesSongs);
